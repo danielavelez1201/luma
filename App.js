ff00c71a7fc57dc6513b383799076defe5fc7d22
Daniela Velez <danielavelez1201@gmail.com>
2020-06-02 01:14:15 -0400

    Add screen navigation

--- App.js
+++ App.js
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
 
 export default function App() {
     const [enteredSymptom, setEnteredSymptom] = useState('');
@@ -15,27 +19,71 @@ export default function App() {
         ]);
     };
 
-    return (
-        <View style={styles.screen}>
-            <View style={styles.inputContainer}>
-                <TextInput placeholder="Symptom"
-                    style={styles.input}
-                    onChangeText={symptomInputHandler}
-                    value={enteredSymptom}
+    const Stack = createStackNavigator();
+
+
+    function HomeScreen({ navigation }) {
+        return (
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                <Text>Home Screen</Text>
+                <Button
+                    title="Go to Details"
+                    onPress={() => navigation.navigate('Details')}
                 />
-                <Button title="ADD" onPress={addSymptomHandler} />
             </View>
-            <FlatList
-                keyExtractor={(item, index) => item.key}
-                data={symptoms}
-                renderItem={itemData => (
-                    <View style={styles.listItem}>
-                        <Text>{itemData.item.value}</Text>
-                    </View>
-                )} />
-        </View>
+        );
+    }
+
+    function Details({ navigation }) {
+        return (
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                <Text>Home Screen</Text>
+                <Button
+                    title="Go to Details"
+                    onPress={() => navigation.navigate('Details')}
+                />
+            </View>
+        );
+    }
+
+    function symptomLogger({ navigation }) {
+        return (
+            <View style={styles.screen}>
+                <View style={styles.inputContainer}>
+                    <TextInput placeholder="Symptom"
+                        style={styles.input}
+                        onChangeText={symptomInputHandler}
+                        value={enteredSymptom}
+                    />
+                    <Button title="ADD" onPress={addSymptomHandler} />
+                </View>
+                <FlatList
+                    keyExtractor={(item, index) => item.key}
+                    data={symptoms}
+                    renderItem={itemData => (
+                        <View style={styles.listItem}>
+                            <Text>{itemData.item.value}</Text>
+                        </View>
+                    )} />
+            </View>
+        )
+    }
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Home" component={HomeScreen} />
+                <Stack.Screen name="Details" component={Details} />
+            </Stack.Navigator>
+        </NavigationContainer>
     );
-}
+    
+
+    }
+
+
+
+         
+    
 
 const styles = StyleSheet.create({
     screen: {
@@ -61,3 +109,4 @@ const styles = StyleSheet.create({
         borderWidth: 1
     }
 });
+
