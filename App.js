580300170b6ed0ceae49ac9dd8d600bc0ba7545f
Daniela Velez <danielavelez1201@gmail.com>
2020-06-02 00:33:40 -0400

    Develop app structure and sample app design code

--- App.js
+++ App.js
@@ -1,19 +1,63 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+    const [enteredSymptom, setEnteredSymptom] = useState('');
+    const [symptoms, setSymptoms] = useState([]);
+
+    const symptomInputHandler = (enteredText) => {
+        setEnteredSymptom(enteredText);
+    }
+
+    const addSymptomHandler = () => {
+        setSymptoms(currentSymptoms => [...currentSymptoms,
+        { key: Math.random().toString(), value: enteredSymptom }
+        ]);
+    };
+
+    return (
+        <View style={styles.screen}>
+            <View style={styles.inputContainer}>
+                <TextInput placeholder="Symptom"
+                    style={styles.input}
+                    onChangeText={symptomInputHandler}
+                    value={enteredSymptom}
+                />
+                <Button title="ADD" onPress={addSymptomHandler} />
+            </View>
+            <FlatList
+                keyExtractor={(item, index) => item.key}
+                data={symptoms}
+                renderItem={itemData => (
+                    <View style={styles.listItem}>
+                        <Text>{itemData.item.value}</Text>
+                    </View>
+                )} />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    screen: {
+        padding: 80
+
+    },
+    inputContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    input: {
+        width: '80%',
+        borderBottomColor: 'black',
+        borderBottomWidth: 1,
+        padding: 10
+    },
+    listItem: {
+        padding: 10,
+        marginVertical: 10,
+        backgroundColor: '#ccc',
+        borderColor: 'black',
+        borderWidth: 1
+    }
 });
